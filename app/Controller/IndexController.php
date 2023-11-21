d0269f2d59929a196dff4b964cc137cb21b1c54a
RZH-EVANE\80961 <809611051@qq.com>
2023-11-21 09:31:38 +0800

    xx

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -24,7 +24,7 @@ class IndexController extends AbstractController
         $user = $this->request->input('user', 'Hyperf');
         $method = $this->request->getMethod();
 
-        return $render->render('index','wo');
+        return $render->render('index');
 //        return $this->view->render('index',['name' => $user]);
 //        return [
 //            'method' => $method,
