621eda68a51ad4b1d7247ab7a56d0a2f3a2e6018
RZH-EVANE\80961 <809611051@qq.com>
2023-11-23 09:19:33 +0800

    chat

--- app/Helper/helpv2.php
+++ app/Helper/helpv2.php
@@ -8,7 +8,7 @@ declare(strict_types = 1);
 
 namespace App\Helper;
 
-if (function_exists('jsonSuccessV2')) {
+if (!function_exists('jsonSuccessV2')) {
     function jsonSuccessV2($data = [], $code = 0, $msg = 'success')
     {
         $result = [
