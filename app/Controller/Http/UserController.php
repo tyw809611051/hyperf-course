1b15d280c5c6216ea02247898615a98eb21a5e2d
RZH-EVANE\80961 <809611051@qq.com>
2023-11-23 16:05:55 +0800

    chat

--- app/Controller/Http/UserController.php
+++ app/Controller/Http/UserController.php
@@ -91,7 +91,7 @@ class UserController extends CommonController
             }
             $regRs = UserService::register($email,$password);
         } catch (\Exception $e) {
-            return $this->resp->error($e->getCode());
+            return $this->resp->error(intval($e->getCode()));
         }
 
         if (!$regRs) {
