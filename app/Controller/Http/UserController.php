e66fa44011f9f4c1fd8040fbb45e446f23236379
RZH-EVANE\80961 <809611051@qq.com>
2023-11-24 11:22:35 +0800

    chat

--- app/Controller/Http/UserController.php
+++ app/Controller/Http/UserController.php
@@ -1,128 +1,126 @@
 <?php
-declare(strict_types = 1);
 
+declare(strict_types=1);
 /**
  * This file is part of Hyperf.
  *
  * @link     https://www.hyperf.io
- * @document https://doc.hyperf.io
+ * @document https://hyperf.wiki
  * @contact  group@hyperf.io
- * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
 namespace App\Controller\Http;
 
-use App\Component\Response;
 use App\Constants\ErrorCode;
-use App\Controller\AbstractController;
 use App\Exception\ApiException;
-use App\Exception\InputException;
 use App\Service\UserService;
+use Exception;
 use Hyperf\Di\Annotation\Inject;
-use Hyperf\HttpMessage\Cookie\Cookie;
 use Hyperf\HttpServer\Annotation\AutoController;
 use Hyperf\HttpServer\Annotation\PostMapping;
 use Hyperf\HttpServer\Annotation\RequestMapping;
 use Hyperf\Validation\Contract\ValidatorFactoryInterface;
 use InvalidArgumentException;
 use Phper666\JWTAuth\JWT;
-use function App\Helper\checkAuth;
-use function App\Helper\jsonError;
-use function App\Helper\jsonSuccess;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
 
-#[AutoController(prefix: "user")]
+use function App\Helper\checkAuth;
+
+#[AutoController(prefix: 'user')]
 class UserController extends CommonController
 {
+    #[Inject]
+    protected ValidatorFactoryInterface $validationFactory;
+
+    #[Inject]
+    protected LoggerInterface $logger;
 
     #[Inject]
     private JWT $jwt;
 
-    #[Inject]
-    protected ValidatorFactoryInterface $validationFactory;
-
     /**
-     * 登录
-     * @return ResponseInterface
+     * 登录.
      * @throws InvalidArgumentException
-     *
      */
-    #[RequestMapping(path: "login",methods: "POST")]
+    #[RequestMapping(path: 'login', methods: 'POST')]
     public function login(): ResponseInterface
     {
         $email = $this->request->input('email');
-        $password   = $this->request->input('password');
+        $password = $this->request->input('password');
         try {
-            $user  = UserService::login($email,$password);
+            $user = UserService::login($email, $password);
             $auth = [
-                'uid'=>$user->id,
-                'username'=>$user->email,
+                'uid' => $user->id,
+                'username' => $user->email,
             ];
-            $token = $this->jwt->getToken('default',$auth);
+            $token = $this->jwt->getToken('default', $auth);
 
             return $this->resp->success([
-                'token'=>$token->toString(),
+                'token' => $token->toString(),
                 'exp' => $this->jwt->getTTL($token->toString()),
-                'uid'=>$user->id,
+                'uid' => $user->id,
             ]);
-        } catch (\Exception $e) {
-            return $this->resp->error(intval($e->getCode()),$e->getMessage());
+        } catch (Exception $e) {
+            return $this->resp->error(intval($e->getCode()), $e->getMessage());
         }
     }
 
     /**
-     * 注册
-     * @return ResponseInterface
+     * 注册.
      * */
-    #[PostMapping(path: "register")]
+    #[PostMapping(path: 'register')]
     public function register(): ResponseInterface
     {
         $email = $this->request->input('email');
         $password = $this->request->input('password');
         $params = $this->request->all();
-        //---------参数校验---------//
-        $validator = $this->validationFactory->make($params,[
-            'email'    => 'required|email|max:50',
+        // ---------参数校验---------//
+        $validator = $this->validationFactory->make($params, [
+            'email' => 'required|email|max:50',
             'password' => 'required|string|max:50',
         ]);
         try {
             if ($validator->fails()) {
                 $errMsg = array_values($validator->errors()->all());
 
-                throw new ApiException(ErrorCode::PARAM_ERROR,implode(',', $errMsg));
+                throw new ApiException(ErrorCode::PARAM_ERROR, implode(',', $errMsg));
             }
-            $regRs = UserService::register($email,$password);
-        } catch (\Exception $e) {
-            return $this->resp->error(intval($e->getCode()),$e->getMessage());
+            $regRs = UserService::register($email, $password);
+        } catch (Exception $e) {
+            return $this->resp->error(intval($e->getCode()), $e->getMessage());
         }
 
-        if (!$regRs) {
+        if (! $regRs) {
             return $this->resp->error(ErrorCode::USER_EMAIL_ALREADY_USE);
         }
         return $this->resp->success();
     }
 
     /**
-     * home
+     * home.
      * */
-    #[RequestMapping(path: "home", methods: "GET")]
+    #[RequestMapping(path: 'home', methods: 'GET')]
     public function home(): ResponseInterface
     {
         try {
-            if (!$user = checkAuth()) {
+            if (! $user = checkAuth()) {
+                $this->logger->info('user: ', $user);
                 return $this->resp->redirect(env('APP_URL') . '/index/login');
             }
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
+            $this->logger->info('user exception: ', $e->getMessage());
             return $this->resp->redirect(env('APP_URL') . '/index/login');
         }
 
         $menus = \Hyperf\Config\Config('menu');
-        return $this->view->render('user/home',[
-            'menus'      => $menus,
-            'user'       => $user,
-            'wsUrl'      => env('WS_URL'),
-            'webRtcUrl'  => env('WEB_RTC_URL'),
-            'stunServer' => 'stunServer'
+        return $this->view->render('user/home', [
+            'menus' => $menus,
+            'user' => $user,
+            'wsUrl' => env('WS_URL'),
+            'webRtcUrl' => env('WEB_RTC_URL'),
+            'stunServer' => 'stunServer',
         ]);
     }
 }
