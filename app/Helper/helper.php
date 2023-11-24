7d6cf8bb936becd6d9000fe63dd39ecddc2c63de
RZH-EVANE\80961 <809611051@qq.com>
2023-11-24 14:39:32 +0800

    chat

--- app/Helper/helper.php
+++ app/Helper/helper.php
@@ -1,5 +1,14 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 
 namespace App\Helper;
 
@@ -7,30 +16,31 @@ use App\Constants\ErrorCode;
 use App\Constants\WsMessage;
 use App\Exception\ApiException;
 use App\Model\User;
-use Hyperf\Context\ApplicationContext;
+use Hyperf\Context\Context;
 use Hyperf\Utils\Codec\Json;
-use \Hyperf\Context\Context;
 use Phper666\JWTAuth\Util\JWTUtil;
 use Psr\Http\Message\ServerRequestInterface;
 
-if (!function_exists('autoHidSubstr')) {
+use const JSON_UNESCAPED_UNICODE;
+
+if (! function_exists('autoHidSubstr')) {
     /**
-     * 智能字符串模糊化
+     * 智能字符串模糊化.
      *
      * @param string $str 被模糊的字符串
-     * @param int    $len 模糊的长度
+     * @param int $len 模糊的长度
      *
      * @return string
      */
     function autoHidSubstr($str, $len = 3)
     {
         if (empty($str)) {
-            return NULL;
+            return null;
         }
-        $str = (string)$str;
+        $str = (string) $str;
 
         $sub_str = mb_substr($str, 0, 1, 'utf-8');
-        for ($i = 0; $i < $len; $i++) {
+        for ($i = 0; $i < $len; ++$i) {
             $sub_str .= '*';
         }
         if (mb_strlen($str, 'utf-8') <= 2) {
@@ -41,23 +51,21 @@ if (!function_exists('autoHidSubstr')) {
     }
 }
 
-if (!function_exists('isEmptyParam')) {
+if (! function_exists('isEmptyParam')) {
     /**
-     * 判断是否存在并且不为空
-     *
-     * @param $param
+     * 判断是否存在并且不为空.
      *
      * @return bool
      */
     function isEmptyParam($param)
     {
-        return (isset($param) && !empty($param));
+        return isset($param) && ! empty($param);
     }
 }
 
-if (!function_exists('getRandStr')) {
+if (! function_exists('getRandStr')) {
     /**
-     * 产生数字与字母混合随机字符串
+     * 产生数字与字母混合随机字符串.
      *
      * @param int $len 数值长度,默认6位
      *
@@ -65,7 +73,7 @@ if (!function_exists('getRandStr')) {
      */
     function getRandStr($len = 6)
     {
-        $chars    = [
+        $chars = [
             'a',
             'b',
             'c',
@@ -132,7 +140,7 @@ if (!function_exists('getRandStr')) {
         $charsLen = count($chars) - 1;
         shuffle($chars);
         $output = '';
-        for ($i = 0; $i < $len; $i++) {
+        for ($i = 0; $i < $len; ++$i) {
             $output .= $chars[mt_rand(0, $charsLen)];
         }
 
@@ -140,7 +148,7 @@ if (!function_exists('getRandStr')) {
     }
 }
 
-if (!function_exists('getClientIp')) {
+if (! function_exists('getClientIp')) {
     function getClientIp()
     {
         try {
@@ -170,24 +178,24 @@ if (!function_exists('getClientIp')) {
         return '0.0.0.0';
     }
 }
-if (!function_exists('verifyIp')) {
+if (! function_exists('verifyIp')) {
     function verifyIp($realip)
     {
         return filter_var($realip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
     }
 }
-if (!function_exists('p')) {
-    function p($val, $title = NULL, $starttime = '')
+if (! function_exists('p')) {
+    function p($val, $title = null, $starttime = '')
     {
-        print_r('[ ' . date("Y-m-d H:i:s") . ']:');
-        if ($title != NULL) {
-            print_r("[" . $title . "]:");
+        print_r('[ ' . date('Y-m-d H:i:s') . ']:');
+        if ($title != null) {
+            print_r('[' . $title . ']:');
         }
         print_r($val);
         print_r("\r\n");
     }
 }
-if (!function_exists('uuid')) {
+if (! function_exists('uuid')) {
     function uuid($length)
     {
         if (function_exists('random_bytes')) {
@@ -203,52 +211,48 @@ if (!function_exists('uuid')) {
         return $uuid;
     }
 }
-if (!function_exists('filterEmoji')) {
+if (! function_exists('filterEmoji')) {
     function filterEmoji($str)
     {
-        $str     = preg_replace_callback(
+        $str = preg_replace_callback(
             '/./u',
-            function (array $match)
-            {
+            function (array $match) {
                 return strlen($match[0]) >= 4 ? '' : $match[0];
             },
-            $str);
+            $str
+        );
         $cleaned = strip_tags($str);
-        return htmlspecialchars(($cleaned));
+        return htmlspecialchars($cleaned);
     }
 }
 
-if (!function_exists('convertUnderline')) {
+if (! function_exists('convertUnderline')) {
     function convertUnderline($str)
     {
-        $str = preg_replace_callback('/([-_]+([a-z]{1}))/i', function ($matches)
-        {
+        return preg_replace_callback('/([-_]+([a-z]{1}))/i', function ($matches) {
             return strtoupper($matches[2]);
         }, $str);
-        return $str;
     }
 }
 
 /*
     * 驼峰转下划线
     */
-if (!function_exists('humpToLine')) {
+if (! function_exists('humpToLine')) {
     function humpToLine($str)
     {
-        $str = preg_replace_callback('/([A-Z]{1})/', function ($matches)
-        {
+        return preg_replace_callback('/([A-Z]{1})/', function ($matches) {
             return '_' . strtolower($matches[0]);
         }, $str);
-        return $str;
     }
 }
-if (!function_exists('convertHump')) {
+if (! function_exists('convertHump')) {
     function convertHump(array $data)
     {
         $result = [];
         foreach ($data as $key => $item) {
             if (is_array($item) || is_object($item)) {
-                $result[humpToLine($key)] = convertHump((array)$item);
+                $result[humpToLine($key)] = convertHump((array) $item);
             } else {
                 $result[humpToLine($key)] = $item;
             }
@@ -257,51 +261,50 @@ if (!function_exists('convertHump')) {
     }
 }
 
-/**
+/*
  * @param $list
  * @param $x
  * @param $y
  *
  * @return bool
  */
-if (!function_exists('inRegion')) {
+if (! function_exists('inRegion')) {
     function inRegion($list, $x, $y)
     {
         $count = count($list);
-        $c     = 0;
-        for ($i = 0; $i < $count; $i++) {
+        $c = 0;
+        for ($i = 0; $i < $count; ++$i) {
             [$x1, $y1] = $list[$i];
             [$x2, $y2] = $list[($i + 1) % $count];
             if ($y1 == $y2) {
                 continue;
-            }//平行线不计算
+            }// 平行线不计算
             if ($y < \min($y1, $y2)) {
                 continue;
-            }//上下区域外不计算
+            }// 上下区域外不计算
             if ($y > \max($y1, $y2)) {
                 continue;
-            }//上下区域外不计算
+            }// 上下区域外不计算
             $j = $x2 + (($x1 - $x2) * ($y - $y2) / ($y1 - $y2));
             if ($j < $x) {
                 continue;
             }
             if ($j == $x2 && $y == $y2) {
                 continue;
-            }//同一顶点只能计算一次
+            }// 同一顶点只能计算一次
             if ($j == $x) {
                 return true;
             }
-            $c++;
+            ++$c;
         }
         return $c % 2 == 1;
     }
 }
-if (!function_exists('toArray()')) {
+if (! function_exists('toArray()')) {
     /**
-     * 任意对象转为数组，确保返回的结果是数组
+     * 任意对象转为数组，确保返回的结果是数组.
      *
-     * @param        $data
-     * @param string $key   如果key不为空，重定义二维数组的key值为二维中的某一列值
+     * @param string $key 如果key不为空，重定义二维数组的key值为二维中的某一列值
      * @param string $value 如果value不为空，第二维数组变为一列值
      *
      * @return array|mixed
@@ -310,13 +313,13 @@ if (!function_exists('toArray()')) {
     {
         if (\is_array($data)) {
             $r = $data;
-        } elseif (!$data) {
+        } elseif (! $data) {
             $r = [];
         } elseif (\method_exists($data, 'toArray')) {
             $r = $data->toArray() ?? [];
         } elseif (\is_object($data)) {
-            $json = \json_encode($data, \JSON_UNESCAPED_UNICODE);
-            $r    = \json_decode($json, true) ?? [];
+            $json = \json_encode($data, JSON_UNESCAPED_UNICODE);
+            $r = \json_decode($json, true) ?? [];
         } elseif (\is_string($data)) {
             $r = \json_decode($data, true) ?? [];
         } else {
@@ -325,7 +328,7 @@ if (!function_exists('toArray()')) {
         if ($key) {
             $r2 = [];
             foreach ($r as $k => $v) {
-                if (!empty($v[$key])) {
+                if (! empty($v[$key])) {
                     $r2[$v[$key]] = $value ? ($v[$value] ?? '') : $v;
                 }
             }
@@ -335,7 +338,7 @@ if (!function_exists('toArray()')) {
     }
 }
 
-if (!function_exists('checkAuth')) {
+if (! function_exists('checkAuth')) {
     /**
      * @return \Hyperf\Database\Model\Builder|\Hyperf\Database\Model\Model|object
      * @throws \Psr\SimpleCache\InvalidArgumentException
@@ -347,73 +350,69 @@ if (!function_exists('checkAuth')) {
          * @var ServerRequestInterface $request
          */
         $request = Context::get(ServerRequestInterface::class);
-        $token   = $request->getCookieParams()['IM_TOKEN'] ?? '';
+        $token = $request->getCookieParams()['IM_TOKEN'] ?? '';
         if (strlen($token) > 0) {
             try {
-//                $jwt = make(JWT::class);
-                $jwt = ApplicationContext::getContainer()->get(JWT::class);
                 if (strlen($token) > 0) {
-//                    $jwtData = $jwt->getParserData($token);
                     $jwtData = JWTUtil::getParserData($request);
-                    $user    = User::query()->where(['id' => $jwtData['uid']])->first();
+                    $user = User::query()->where(['id' => $jwtData['uid']])->first();
                     if (empty($user)) {
                         throw new ApiException(ErrorCode::AUTH_ERROR);
                     }
-                    return $user ?? NULL;
+                    return $user ?? null;
                 }
             } catch (Throwable $e) {
                 throw new ApiException(ErrorCode::AUTH_ERROR);
             }
         }
-        return NULL;
+        return null;
     }
 }
 
-if (!function_exists('wsSuccess')) {
+if (! function_exists('wsSuccess')) {
     function wsSuccess($cmd = WsMessage::WS_MESSAGE_CMD_EVENT, $method = '', $data = [], $msg = 'Success')
     {
         $result = [
-            'cmd'    => $cmd,
+            'cmd' => $cmd,
             'method' => $method,
-            'msg'    => $msg,
-            'data'   => $data
+            'msg' => $msg,
+            'data' => $data,
         ];
 
         return Json::encode($result);
     }
 }
 
-if (!function_exists('wsError')) {
+if (! function_exists('wsError')) {
     function wsError($msg = 'Error', $cmd = WsMessage::WS_MESSAGE_CMD_ERROR, $data = [])
     {
         $result = [
-            'cmd'  => $cmd,
-            'msg'  => $msg,
-            'data' => $data
+            'cmd' => $cmd,
+            'msg' => $msg,
+            'data' => $data,
         ];
         return Json::encode($result);
     }
 }
 
-if (!function_exists('jsonSuccess')) {
-    function jsonSuccess($data = NULL, int $code = 0, string $message = 'success')
+if (! function_exists('jsonSuccess')) {
+    function jsonSuccess($data = null, int $code = 0, string $message = 'success')
     {
         return [
-            'data'   => $data,
+            'data' => $data,
             'code' => $code,
-            'msg'    => $message,
+            'msg' => $message,
         ];
     }
 }
 
-if (!function_exists('jsonError')) {
-    function jsonError($code, $msg = 'error', $data=[])
+if (! function_exists('jsonError')) {
+    function jsonError($code, $msg = 'error', $data = [])
     {
         return [
-            'data'  => $data,
-            'msg'  => $msg,
-            'code' => $code
+            'data' => $data,
+            'msg' => $msg,
+            'code' => $code,
         ];
     }
 }
-
