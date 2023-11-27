4546adaac73f4120c86255923d56161fd83a908f
RZH-EVANE\80961 <809611051@qq.com>
2023-11-27 14:49:31 +0800

    chat

--- app/Middleware/JwtAuthMiddleware.php
+++ app/Middleware/JwtAuthMiddleware.php
@@ -71,7 +71,7 @@ class JwtAuthMiddleware implements MiddlewareInterface
         }
 
         if ($isValidToken) {
-            $request = $request->withAddedHeader('Authorization', 'Bearer ' . $token);
+            //            $request = $request->withAddedHeader('Authorization', 'Bearer ' . $token);
             $jwtData = JWTUtil::getParserData($request);
             $user = User::query()->where(['id' => $jwtData['uid']])->first();
             if (empty($user)) {
