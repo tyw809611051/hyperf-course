8686e61f272f798b86cece964be9c24f03761474
RZH-EVANE\80961 <809611051@qq.com>
2023-11-28 15:16:05 +0800

    chat

--- app/Constants/ErrorCode.php
+++ app/Constants/ErrorCode.php
@@ -1,13 +1,13 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 /**
  * This file is part of Hyperf.
  *
  * @link     https://www.hyperf.io
- * @document https://doc.hyperf.io
+ * @document https://hyperf.wiki
  * @contact  group@hyperf.io
- * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
 namespace App\Constants;
@@ -17,92 +17,113 @@ use Hyperf\Constants\Annotation\Constants;
 
 /**
  * @Constants
- * @method  static getMessage(int $code)
+ * @method static getMessage(int $code)
  */
 class ErrorCode extends AbstractConstants
 {
-    const SUCCESS = 200;   // 接口处理成功
+    public const SUCCESS = 0;   // 接口处理成功
 
-    //基本错误码 0～1000
-    const
-        PARAM_ERROR = 301,
-        AUTH_ERROR = 401;
+    // 基本错误码 0～1000
+    public const PARAM_ERROR = 301;
 
+    public const AUTH_ERROR = 401;
 
-    //用户错误码 3000～3999
+    // 用户错误码 3000～3999
 
-    const
-        USER_NOT_FOUND = 3001,
-        USER_ID_INVALID = 3002,
-        USER_EMAIL_ALREADY_USE = 3003,
-        USER_PASSWORD_ERROR = 3004,
-        USER_CREATE_APPLICATION_FAIL = 3005,
-        USER_APPLICATION_SET_READ_FAIL = 3006,
-        USER_INFO_MODIFY_FAIL = 3007,
-        USER_APPLICATION_NOT_FOUND = 3008,
-        USER_APPLICATION_PROCESSED = 3009,
-        USER_APPLICATION_TYPE_WRONG = 3010,
-        USER_IN_VIDEO_CALL = 3011;
+    public const USER_NOT_FOUND = 3001;
 
-    const FRIEND_GROUP_CREATE_FAIL = 4001,
-        FRIEND_GROUP_NOT_FOUND = 4002,
-        FRIEND_NOT_FOUND = 4003,
-        FRIEND_NOT_ADD_SELF = 4004,
-        FRIEND_RELATION_ALREADY = 4005,
-        FRIEND_CALL_IN_PROGRESS = 4006;
+    public const USER_ID_INVALID = 3002;
 
-    const GROUP_CREATE_FAIL = 5001,
-        GROUP_NOT_FOUND = 5002,
-        GROUP_RELATION_CREATE_FAIL = 5010,
-        GROUP_RELATION_ALREADY = 5011,
-        GROUP_FULL = 5012,
-        GROUP_NOT_MEMBER = 5013;
+    public const USER_EMAIL_ALREADY_USE = 3003;
+
+    public const USER_PASSWORD_ERROR = 3004;
+
+    public const USER_CREATE_APPLICATION_FAIL = 3005;
+
+    public const USER_APPLICATION_SET_READ_FAIL = 3006;
+
+    public const USER_INFO_MODIFY_FAIL = 3007;
+
+    public const USER_APPLICATION_NOT_FOUND = 3008;
+
+    public const USER_APPLICATION_PROCESSED = 3009;
+
+    public const USER_APPLICATION_TYPE_WRONG = 3010;
+
+    public const USER_IN_VIDEO_CALL = 3011;
+
+    public const FRIEND_GROUP_CREATE_FAIL = 4001;
+
+    public const FRIEND_GROUP_NOT_FOUND = 4002;
+
+    public const FRIEND_NOT_FOUND = 4003;
+
+    public const FRIEND_NOT_ADD_SELF = 4004;
+
+    public const FRIEND_RELATION_ALREADY = 4005;
+
+    public const FRIEND_CALL_IN_PROGRESS = 4006;
+
+    public const GROUP_CREATE_FAIL = 5001;
+
+    public const GROUP_NOT_FOUND = 5002;
+
+    public const GROUP_RELATION_CREATE_FAIL = 5010;
+
+    public const GROUP_RELATION_ALREADY = 5011;
+
+    public const GROUP_FULL = 5012;
+
+    public const GROUP_NOT_MEMBER = 5013;
 
     // ext 9000~9999
-    const
-        JWT_PRIVATE_KEY_EMPTY = 9001,
-        JWT_PUBLIC_KEY_EMPTY = 9002,
-        JWT_ALG_EMPTY = 9003,
-        NO_PERMISSION_PROCESS = 9004,
-        CONFIG_NOT_FOUND = 9005,
-        FILE_DOES_NOT_EXIST = 9006;
+    public const JWT_PRIVATE_KEY_EMPTY = 9001;
+
+    public const JWT_PUBLIC_KEY_EMPTY = 9002;
+
+    public const JWT_ALG_EMPTY = 9003;
+
+    public const NO_PERMISSION_PROCESS = 9004;
+
+    public const CONFIG_NOT_FOUND = 9005;
+
+    public const FILE_DOES_NOT_EXIST = 9006;
 
     public static $errorMessages = [
-
         self::PARAM_ERROR => '请求参数有误',
         self::AUTH_ERROR => 'Authorization has been denied for this request !',
 
-        self::USER_NOT_FOUND                 => 'User not found!',
-        self::USER_ID_INVALID                => 'The user id is invalid !',
-        self::USER_EMAIL_ALREADY_USE         => 'This mailbox is already in use !',
-        self::USER_PASSWORD_ERROR            => 'User password input error !',
-        self::USER_CREATE_APPLICATION_FAIL   => 'Failed to create user application !',
+        self::USER_NOT_FOUND => 'User not found!',
+        self::USER_ID_INVALID => 'The user id is invalid !',
+        self::USER_EMAIL_ALREADY_USE => 'This mailbox is already in use !',
+        self::USER_PASSWORD_ERROR => 'User password input error !',
+        self::USER_CREATE_APPLICATION_FAIL => 'Failed to create user application !',
         self::USER_APPLICATION_SET_READ_FAIL => 'application set to read failed',
-        self::USER_INFO_MODIFY_FAIL          => 'Failed to modify user information !',
-        self::USER_APPLICATION_NOT_FOUND     => 'Application information does not exist !',
-        self::USER_APPLICATION_PROCESSED     => 'Application information has been processed !',
-        self::USER_APPLICATION_TYPE_WRONG    => 'Wrong application type !',
-        self::USER_IN_VIDEO_CALL             => '您正在视频通话中！', // You are in a video call
+        self::USER_INFO_MODIFY_FAIL => 'Failed to modify user information !',
+        self::USER_APPLICATION_NOT_FOUND => 'Application information does not exist !',
+        self::USER_APPLICATION_PROCESSED => 'Application information has been processed !',
+        self::USER_APPLICATION_TYPE_WRONG => 'Wrong application type !',
+        self::USER_IN_VIDEO_CALL => '您正在视频通话中！', // You are in a video call
 
         self::FRIEND_GROUP_CREATE_FAIL => 'Friend group creation failed !',
-        self::FRIEND_GROUP_NOT_FOUND   => 'Friend group not found !',
-        self::FRIEND_NOT_FOUND         => 'Friend not found!',
-        self::FRIEND_NOT_ADD_SELF      => 'You can\'t add yourself as a friend !',
-        self::FRIEND_RELATION_ALREADY  => 'You\'re already friends !',
-        self::FRIEND_CALL_IN_PROGRESS  => '对方正在视频通话中', // Video call in progress
+        self::FRIEND_GROUP_NOT_FOUND => 'Friend group not found !',
+        self::FRIEND_NOT_FOUND => 'Friend not found!',
+        self::FRIEND_NOT_ADD_SELF => 'You can\'t add yourself as a friend !',
+        self::FRIEND_RELATION_ALREADY => 'You\'re already friends !',
+        self::FRIEND_CALL_IN_PROGRESS => '对方正在视频通话中', // Video call in progress
 
-        self::GROUP_CREATE_FAIL          => 'Group creation failed !',
-        self::GROUP_NOT_FOUND            => 'Group not found !',
+        self::GROUP_CREATE_FAIL => 'Group creation failed !',
+        self::GROUP_NOT_FOUND => 'Group not found !',
         self::GROUP_RELATION_CREATE_FAIL => 'Group relation creation failed !',
-        self::GROUP_RELATION_ALREADY     => 'You\'re already a member of the group !',
-        self::GROUP_FULL                 => 'Group full !',
-        self::GROUP_NOT_MEMBER           => 'You are not a member of this group !',
+        self::GROUP_RELATION_ALREADY => 'You\'re already a member of the group !',
+        self::GROUP_FULL => 'Group full !',
+        self::GROUP_NOT_MEMBER => 'You are not a member of this group !',
 
         self::JWT_PRIVATE_KEY_EMPTY => 'The private key is invalid !',
-        self::JWT_PUBLIC_KEY_EMPTY  => 'The public key is invalid !',
-        self::JWT_ALG_EMPTY         => 'The alg is invalid !',
+        self::JWT_PUBLIC_KEY_EMPTY => 'The public key is invalid !',
+        self::JWT_ALG_EMPTY => 'The alg is invalid !',
         self::NO_PERMISSION_PROCESS => 'No permission to process !',
-        self::CONFIG_NOT_FOUND      => 'Configuration not found !',
-        self::FILE_DOES_NOT_EXIST   => 'File does not exist !'
+        self::CONFIG_NOT_FOUND => 'Configuration not found !',
+        self::FILE_DOES_NOT_EXIST => 'File does not exist !',
     ];
 }
--- public/chat/js/test.js
+++ public/chat/js/test.js
@@ -1,17 +1,65 @@
+import {
+    user_init, static_user_application, static_user_chat_history, group_get_relation, util_upload_img, util_upload_file
+} from "./api.js";
+import {getCookie} from "./util.js";
 //以下为 layim 最新版写法
 layui.config({
-    layimPath: '../../layim/dist/' //配置 layim.js 所在目录
-    ,layimAssetsPath: '../../layim/dist/layim-assets/' //layim 资源文件所在目录
+    layimPath: '../../dist/' //配置 layim.js 所在目录
+    ,layimAssetsPath: '../../dist/layim-assets/' //layim 资源文件所在目录
 }).extend({
     layim: layui.cache.layimPath + 'layim' //配置 layim 组件所在的路径
 }).use('layim', function(layim){ //加载组件
-                                 //先来个客服模式压压精
+                                 //基础配置
     layim.config({
-        brief: true //是否简约模式（如果true则不显示主面板）
-    }).chat({
-        name: '一个新窗口'
-        ,type: 'friend'
-        ,avatar: 'http://tp1.sinaimg.cn/5619439268/180/40030060651/1'
-        ,id: -2
+        init: {
+            url: user_init,
+            type: 'get',
+            data: {
+                token: getCookie('IM_TOKEN')
+            },
+        } //获取主面板列表信息，下文会做进一步介绍
+
+        //获取群员接口（返回的数据格式见下文）
+        ,members: {
+            url: '' //接口地址（返回的数据格式见下文）
+            ,type: 'get' //默认get，一般可不填
+            ,data: {} //额外参数
+        }
+
+        //上传图片接口（返回的数据格式见下文），若不开启图片上传，剔除该项即可
+        ,uploadImage: {
+            url: '' //接口地址
+            ,type: 'post' //默认post
+        }
+
+        //上传文件接口（返回的数据格式见下文），若不开启文件上传，剔除该项即可
+        ,uploadFile: {
+            url: '' //接口地址
+            ,type: 'post' //默认post
+        }
+        //扩展工具栏，下文会做进一步介绍（如果无需扩展，剔除该项即可）
+        ,tool: [{
+            alias: 'code' //工具别名
+            ,title: '代码' //工具名称
+            ,icon: '' //工具图标，参考图标文档
+        }]
+
+        ,msgbox: layui.cache.dir + 'css/modules/layim/html/msgbox.html' //消息盒子页面地址，若不开启，剔除该项即可
+        ,find: layui.cache.dir + 'css/modules/layim/html/find.html' //发现页面地址，若不开启，剔除该项即可
+        ,chatLog: layui.cache.dir + 'css/modules/layim/html/chatlog.html' //聊天记录页面地址，若不开启，剔除该项即可
+        ,brief: false // 是否简约模式，如果设为 true，则主面板不会显示
+        ,title: "我的聊天" // 主面板最小化后显示的名称
+        ,min: false // 用于设定主面板是否在页面打开时，始终最小化展现
+        ,right: "0px" // 用于设定主面板右偏移量。该参数可避免遮盖你页面右下角已经的bar
+        ,minRight: "20px" // 用户控制聊天面板最小化时、及新消息提示层的相对right的px坐标
+        ,initSkin: "5.jpg" // 设置初始背景，默认不开启。可设置./css/modules/layim/skin目录下的图片文件名
+        ,isAudio: false // 是否开启聊天工具栏音频
+        ,isVideo: false // 是否开启开启聊天工具栏视频
+        ,notice: false // 是否开启桌面消息提醒，即在浏览器之外的提醒
+        ,voice: "default.mp3" // 设定消息提醒的声音文件（所在目录：./layui/css/modules/layim/voice/）
+        ,isfriend: true // 是否开启好友
+        ,isgroup: true // 是否开启群组
+        ,maxLength: 3000 // 可允许的消息最大字符长度
+        ,copyright: true,
     });
 });
