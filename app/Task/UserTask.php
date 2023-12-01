4fb3a4baabb940af82ce93c6b5967069d352312b
RZH-EVANE\80961 <809611051@qq.com>
2023-12-01 11:15:43 +0800

    chat

--- app/Task/UserTask.php
+++ app/Task/UserTask.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 namespace App\Task;
 
+use App\Component\Server;
 use App\Constants\WsMessage;
 use Hyperf\WebSocketServer\Sender;
 
@@ -24,5 +25,15 @@ class UserTask
         $result = wsSuccess(WsMessage::WS_MESSAGE_CMD_EVENT, WsMessage::EVENT_GET_UNREAD_APPLICATION_COUNT, $data);
         $sender = \Hyperf\Support\make(Sender::class);
         $sender->push($fd, $result);
+    }
+
+    public function setUserStatus(array $fds, array $data)
+    {
+        if (empty($fds)) {
+            return false;
+        }
+        $result = wsSuccess(WsMessage::WS_MESSAGE_CMD_EVENT, WsMessage::EVENT_USER_STATUS, $data);
+        Server::sendToAll($result, $fds);
+        return true;
     }
 }
