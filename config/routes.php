17b9fb9fa684944d2f460af3fd96d29dd0c0de80
RZH-EVANE\80961 <809611051@qq.com>
2023-12-01 11:07:02 +0800

    chat

--- config/routes.php
+++ config/routes.php
@@ -10,12 +10,11 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 use App\Middleware\AuthMiddleware;
-use App\Middleware\VideoAuthMiddleware;
 use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\Http\HomeController@index');
 
 Router::addServer('ws', function () {
-    Router::get('/im', 'App\Controller\Ws\WebSocketController');
+    Router::get('/im', 'App\Controller\Ws\WebSocketController', ['middleware' => [AuthMiddleware::class]]);
     Router::get('/video', 'App\Controller\Ws\VideoController');
 });
